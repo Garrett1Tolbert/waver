af26c1c63a226b59b4fe33b281682be0a3a9416b
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-10-30 17:22:33 -0500

    chore: updated contract address

--- constants/index.ts
+++ constants/index.ts
@@ -1,3 +1,3 @@
 export const waveContractAddress = '0x980f53ded0fe7122b02cadea5b72e0401f368777';
 export const photosContractAddress =
-	'0x718076ceCf5B14C0561Ff01f9566e22A174B0ce9';
+	'0x484239ad541677Ad8652c56871270D3913A90040';
