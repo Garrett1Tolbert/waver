cb225f25e91c94cb913d9dea5c5319109227836e
Garrett Tolbert <garrett1.tolbert@gmail.com>
2021-10-30 21:09:32 -0500

    refactor: updated states

--- components/molecules/DateList.tsx
+++ components/molecules/DateList.tsx
@@ -2,10 +2,18 @@ import { useData } from '@/hooks/useData';
 import { Stack, Text } from '@chakra-ui/react';
 import { format } from 'date-fns';
 import { useEffect, useState } from 'react';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 export default function DateList(): JSX.Element {
 	const [dates, setDates] = useState<string[]>([]);
 	const { photos } = useData();
+	const router = useRouter();
+	const { date } = router.query;
+
+	const linkBg = 'rgba(255,255,255,.05)';
+	const hoverBg = 'rgba(255,255,255,.1)';
+	const activeBg = 'rgba(255,255,255,.35)';
 
 	useEffect(() => {
 		const temp = photos?.map((photo: any) =>
@@ -15,21 +23,46 @@ export default function DateList(): JSX.Element {
 	}, [photos]);
 
 	return (
-		<Stack mt="6">
-			{dates?.map((date, index) => {
+		<Stack mt="16" overflowY="scroll" pos="relative" h="calc(100% - 189px)">
+			<Text
+				fontSize="lg"
+				color="white"
+				pos="sticky"
+				top={0}
+				bg="#1a202c"
+				fontWeight="medium"
+			>
+				History
+			</Text>
+
+			<Link passHref href="/">
+				<Text
+					py="1"
+					px="3"
+					rounded="md"
+					cursor="pointer"
+					bg={!date ? activeBg : linkBg}
+					color="white"
+					_hover={{ bg: !date ? activeBg : hoverBg }}
+				>
+					All
+				</Text>
+			</Link>
+			{dates?.map((el, idx) => {
 				return (
-					<Text
-						py="1"
-						px="3"
-						rounded="md"
-						cursor="pointer"
-						bg="rgba(255,255,255,.05)"
-						color="white"
-						key={index}
-						_hover={{ bg: 'rgba(255,255,255,.1)' }}
-					>
-						{date}
-					</Text>
+					<Link key={idx} passHref href={`/history/${el}`}>
+						<Text
+							py="1"
+							px="3"
+							rounded="md"
+							cursor="pointer"
+							bg={date === el ? activeBg : linkBg}
+							color="white"
+							_hover={{ bg: date === el ? activeBg : hoverBg }}
+						>
+							{el}
+						</Text>
+					</Link>
 				);
 			})}
 		</Stack>
